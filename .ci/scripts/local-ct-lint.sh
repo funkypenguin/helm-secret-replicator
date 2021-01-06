311638af82985dec1982504a0a4def586ca49257
David Young <davidy@funkypenguin.co.nz>
2021-01-06 14:22:03 +1300

    Use main branch

--- .ci/scripts/local-ct-lint.sh
+++ .ci/scripts/local-ct-lint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker run --rm -it -w /repo -v `pwd`:/repo quay.io/helmpack/chart-testing ct lint --all --debug
+docker run --rm -it -w /repo -v `pwd`:/repo quay.io/helmpack/chart-testing ct list-changed --config .ci/ct-config.yaml
--- .ci/scripts/local-kube-score.sh
+++ .ci/scripts/local-kube-score.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-helm template --values .ci/values-kube-score.yaml charts/fission-all -f .ci/values-kube-score.yaml | kube-score score - \
+helm template --values .ci/values-kube-score.yaml charts/* -f .ci/values-kube-score.yaml | kube-score score - \
     --ignore-test pod-networkpolicy \
     --ignore-test deployment-has-poddisruptionbudget \
     --ignore-test deployment-has-host-podantiaffinity \
